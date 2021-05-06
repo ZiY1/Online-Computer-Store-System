82a9011317873ea95e30f8fae5009621915e24f8
Bill Davila <51135151+billydavila@users.noreply.github.com>
2021-05-06 08:42:44 -0400

    Add files via upload

--- reset_excel_files_to_start.py
+++ reset_excel_files_to_start.py
@@ -42,6 +42,6 @@ df_items.to_excel("csv_files/items.xlsx", index  = False)
 
 
 tempo_df2 = df_staffs[ df_staffs["Type_user"] == "Super user"] 
-
+tempo_df2['Income'] = 0.00
 tempo_df2.to_excel("csv_files/privileged_users.xlsx",index = False)
 
